2d4b0cf8a16190c0a10700059645f28075e1dda8
Erick Mendes <erickmendesweb@gmail.com>
2022-11-21 10:11:48 -0300

    Data-test para correção automatica implementados

--- src/Posts.js
+++ src/Posts.js
@@ -7,7 +7,7 @@ export default function Posts() {
     return (
         <div className="posts">
             {postagens.map((p) => (
-                <div className="post">
+                <div data-test="post" className="post">
                 <Topo image={p.imagemUsuario} user={p.usuario}/>
                 <Conteudo post={p.postagem}/>
                 <Fundo image={p.imagemCurtida} user={p.usuarioCurtida}/>
@@ -39,7 +39,7 @@ function Conteudo({post}){
     return(
         
         <div className="conteudo">
-              <img src={post} />
+              <img data-test="post-image" src={post} />
             </div>
         
     )
@@ -51,19 +51,19 @@ function Fundo({image, user}){
         <div className="fundo">
             <div className="acoes">
             <div>
-                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon data-test="like-post" name="heart-outline"></ion-icon>
                 <ion-icon name="chatbubble-outline"></ion-icon>
                 <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
             <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
+                <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon>
             </div>
         </div>
 
         <div className="curtidas">
           <img src={image} />
           <div className="texto">
-            Curtido por <strong>{user}</strong> e <strong>outras 101.523 pessoas</strong>
+            Curtido por <strong>{user}</strong> e <strong>outras <span data-test="likes-number">101.523</span> pessoas</strong>
           </div>
         </div>
         </div>
