64185324640943df27f4ed65141875db0df5fcb6
Erick Mendes <erickmendesweb@gmail.com>
2022-11-21 10:05:09 -0300

    Processo dinâmico do usuario concluido

--- src/SideBar.js
+++ src/SideBar.js
@@ -4,7 +4,7 @@ import Usuario from "./Usuario"
 export default function SideBar(){
     return (
         <div className="sidebar">
-            <Usuario/>
+            <Usuario usuario="catanacomics"/>
             <Sugestoes/>
         </div>
     )
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,25 @@
-export default function Usuario() {
+import { useState } from "react"
+
+
+export default function Usuario(props) {
+    const [nome,setNome] = useState("Catana")
+    const [imagem,setImagem] = useState("assets/img/catanacomics.svg")
+
+    function inserirNome(){
+        setNome(prompt("Escreve o nome que deseja"))
+    }
+
+    function inserirImagem(){
+        setImagem(prompt("Insira o link da imagem"))
+    }
     return (
-            <div className="usuario">
-                <img src="assets/img/catanacomics.svg"/>
+            <div data-test="user" className="usuario">
+                <img data-test="profile-image" onClick={inserirImagem} src={imagem}/>
                 <div className="texto">
-                    <strong>catanacomics</strong>
+                    <strong>{props.usuario}</strong>
                     <span>
-                          Catana
-                          <ion-icon name="pencil"></ion-icon>
+                          <span data-test="name">{nome}</span>
+                          <ion-icon data-test="edit-name" name="pencil" onClick={inserirNome}></ion-icon>
                     </span>
                 </div>
             </div>
